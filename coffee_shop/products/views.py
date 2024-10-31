a10c20f58cb858cdcfe1cd350728d7cebd2a7b18
Alexis Martinez <jhonyalexis100@gmail.com>
2024-10-31 17:52:18 -0500

    create the form to login in django

--- coffee_shop/products/views.py
+++ coffee_shop/products/views.py
@@ -1,6 +1,7 @@
 
 from django.views import generic
 from .forms import ProductForm
+from .models import Product
 from django.urls import reverse_lazy
 
 
@@ -12,4 +13,8 @@ class ProductFormView(generic.FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
-    
+
+class ProductListView(generic.ListView):
+    model = Product
+    template_name = 'products/list_product.html'
+    context_object_name = 'products'
