5515698f0bf4ff6a2c5026b0d470688c7727dba0
Alexis Martinez <jhonyalexis100@gmail.com>
2024-11-08 17:12:41 -0500

    Config form to orders and details

--- coffee_shop/orders/views.py
+++ coffee_shop/orders/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render
-from django.views.generic import DeleteView
+from django.urls import reverse_lazy
+from django.views.generic import DeleteView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from .forms import OrderProductForm
 from .models import Order
+
 # Create your views here.
 
-class MyOrderView(DeleteView):
+class MyOrderView(LoginRequiredMixin, DeleteView):
     model = Order
     template_name = "orders/my_order.html"
     context_object_name = "order"
     
     def get_object(self, queryset=None):
-        return Order.objects.filter(is_active=True).first()
+        return Order.objects.filter(is_active=True, user=self.request.user).first()
+
+class CreateOrderProductView(LoginRequiredMixin, CreateView):
+    template_name = "orders/create_order_product.html"
+    form_class = OrderProductForm
+    success_url = reverse_lazy("my_order")
+    
+    def form_valid(self, form):
+        order, _ = Order.objects.get_or_create(
+            is_active = True,
+            user = self.request.user
+        )
+        
+        form.instance.order = order
+        form.instance.quantity = 1
+        form.save()
+        return super().form_valid(form)
